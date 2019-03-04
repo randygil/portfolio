235bd66dbde8b6635e04ee33d7086219bf62e715
Randy Gil <r.gilcamejo@gmail.com>
2019-03-04 01:41:27 -0400

    Ligeros cambios

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,10 +11,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "randygil@webcoding.cl"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "randygil@unerg.edu.ve"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Formulario de Contacto:  $name";
-$body = "Recibiste un nuevo emnsaje.\n\n"."Detalles:\n\nNombre: $name\n\nCorreo: $email\n\nTeléfono: $phone\n\nMensaje:\n$message";
-$header = "From: admin@randygil.com.ve\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$body = "Recibiste un nuevo enesaje.\n\n"."Detalles:\n\nNombre: $name\n\nCorreo: $email\n\nTeléfono: $phone\n\nMensaje:\n$message";
+$header = "From: admin@randygil.me\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
